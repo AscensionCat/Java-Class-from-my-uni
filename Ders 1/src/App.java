fd5ba73a4c7599d82743f9a17730acdc82b09288
Yusuf Mert Çınar <ymertc@hotmail.com>
2024-10-23 12:12:12 +0300

    Degisken degistirme ornekkleri ve input

--- Ders 1/src/App.java
+++ Ders 1/src/App.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+        String isim = "Melike";
+
+        System.out.print("Merhaba ");
+        System.out.println(isim);
+
+        System.out.println("lütfen adınızı giriniz");
+        String sayi = input.nextLine();
+        
+        System.out.print("Merhaba ");
+        System.out.println(sayi);
+
+        System.out.println("Nasılsın?");
+
+        System.out.println("Ne yapıyorsun?");
+
+        System.out.println("Lütfen 2 adet sayı giriniz.");
+
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = (((a+2)*b) + (a*(b+3)))*2/2;
+
+        System.out.println(c);
+
     }
 }
